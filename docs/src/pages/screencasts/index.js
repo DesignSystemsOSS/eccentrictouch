4cbc2589f035cfece5d84c97aec2d2ec76f6eba1
yashsehgal <yashsehgal.work@gmail.com>
2021-11-22 01:01:32 +0530

    feat: screen-casts page added :rocket:

--- docs/src/pages/screencasts/index.js
+++ docs/src/pages/screencasts/index.js
@@ -4,9 +4,12 @@ import Layout from '@theme/Layout';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styles from '../index.module.css';
+import ReactModal from 'react-modal';
 
 // videos content data from JSON as an object
 import YouTubeVideosData from './__screencasts-data.json';
+
+ReactModal.setAppElement('#__docusaurus');
 
 export default function Screencasts() {
   const [youtubeVideos] = useState(YouTubeVideosData);
@@ -35,33 +38,67 @@ export default function Screencasts() {
         }}
       >
         {youtubeVideos.map((video, index) => (
-          <div className="video-card-content-container" key={index}>
-            <div className="shadow-hoverable video-wrapper-card"
-              style={{
-                width: '340px',
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'center',
-                flexDirection: 'column',
-                borderRadius: '24px',
-                overflow: 'hidden'
-              }}
-            >
-              <img 
-                src={video.thumbnail} 
-                alt={video.title}
-                style={{
-                  width: '100%',
-                  height: '100%'
-                }}
-              />
-            </div>
-            <p className="m-top-4 text-align-center font-weight-500">
-              {video.title}
-            </p>
-          </div>
+          <YoutubeVideoCard key={index} 
+            title={video.title}
+            thumbnail={video.thumbnail}
+          />
         ))}
       </div>
     </Layout>
+  )
+}
+
+function YoutubeVideoCard(__YoutubeVideoDetails) {
+  const [youtubeVideoModalRef, setYoutubeVideoModal] = useState(false);
+  return (
+    <div className="video-card-content-container"
+      onClick={() => setYoutubeVideoModal(true)}
+      key={__YoutubeVideoDetails.index}
+    >
+      <div className="shadow-hoverable video-wrapper-card"
+        style={{
+          width: '340px',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          flexDirection: 'column',
+          borderRadius: '24px',
+          overflow: 'hidden'
+        }}
+      >
+        <img
+          src={__YoutubeVideoDetails.thumbnail}
+          alt={__YoutubeVideoDetails.title}
+          style={{
+            width: '100%',
+            height: '100%'
+          }}
+        />
+      </div>
+      <p className="m-top-4 text-align-center font-weight-500">
+        {__YoutubeVideoDetails.title}
+      </p>
+      <ReactModal 
+        isOpen={youtubeVideoModalRef} 
+        key={__YoutubeVideoDetails.index}
+        onRequestClose={() => setYoutubeVideoModal(false)}
+        style={{
+          overlay: {
+            backgroundColor: 'var(--color-purple-700)'
+          },
+          content: {
+            width: '60%',
+            height: 'fit-content',
+            padding: '1.6em',
+            margin: 'auto',
+            boxShadow: '0px 6px 20px #00000045'
+          }
+        }}
+      >
+        <button className="primary-btn bg-color-red-900"
+          onClick={() => setYoutubeVideoModal(false)}>Back
+        </button>
+      </ReactModal>
+    </div>
   )
 }
